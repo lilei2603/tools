c29d9f050dc3afd124f461fbb0ed6b6869f47317
Lei <240924085@qq.com>
2023-12-15 21:57:27 +0800

    fix: update BaiDuMapADBlock.plugin

--- Loon/BaiDuMap_ADBlock.js
+++ Loon/BaiDuMap_ADBlock.js
@@ -1,6 +1,7 @@
 const body = JSON.parse($response.body);
 body.data.posts.content = [];
 body.data.themes.content = [];
+console.log(JSON.stringify(body));
 $done({
     body: JSON.stringify(body)
 });
